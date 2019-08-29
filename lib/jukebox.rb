ca0594b1688bcbfa23f79f6bb55255ca7f8cc43d
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 18:53:09 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,9 +1,20 @@
 # Add your code here
-def say_hello(name)
-  "Hi #{name}!"
-end
- 
-puts "Enter your name:"
-users_name = gets.strip
- 
-puts say_hello(users_name)
+def help 
+  
+end 
+
+def play 
+  
+end 
+
+def list 
+  
+end 
+
+def exit_jukebox 
+  
+end 
+
+def run 
+  
+end 
