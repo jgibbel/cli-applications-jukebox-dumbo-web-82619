b66426fb825604bd92832474838910ddc4b2659d
James Gibbel <jamesgibbel@gmail.com>
2019-08-29 18:56:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,19 +12,22 @@ songs = [
 ]
 
 def help 
- puts "I accept the following commands:"
-puts "- help : displays this help message"
-puts "- list : displays a list of songs you can play"
-puts "- play : lets you choose a song to play"
-- exit : exits this program
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end 
 
-def play 
+def play(songs)
+  
   
 end 
 
-def list 
-  
+def list(songs)
+  songs.each_with_index do |element, index|
+    puts "#{index+1}. #{element}"
+  end
 end 
 
 def exit_jukebox 
